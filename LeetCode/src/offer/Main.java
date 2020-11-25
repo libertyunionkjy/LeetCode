a6e8d65386b1907a2674247389476df733f442ed
Junyu Ke <libertyunionkjy@gmail.com>
2020-11-26 01:37:06 +0800

    Solution Of https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/

--- LeetCode/src/offer/Main.java
+++ LeetCode/src/offer/Main.java
@@ -4,10 +4,8 @@ import offer.FindNumberIn2DArray;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] matrix = {};
-        int target = 30;
-        FindNumberIn2DArray solution = new FindNumberIn2DArray();
-        System.out.println(solution.findNumberIn2DArray(matrix,target));
-        //System.out.println("行数:"+matrix.length);
+        MinArray solution = new MinArray();
+        solution
+        System.out.println();
     }
 }
--- LeetCode/src/offer/MinArray.java
+++ LeetCode/src/offer/MinArray.java
@@ -1,4 +1,18 @@
 package offer;
 
 public class MinArray {
+    public int minArray(int[] nums) {
+        int res = nums[0];
+        if(nums.length == 1)
+            return nums[0];
+        else{
+            for(int i = 0; i <= nums.length - 2; i++){
+                if(nums[i] > nums[i+1]){
+                    res = nums[i+1];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
 }
