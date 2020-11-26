ba8c7faa77d6039f245200c85ff1de3849daa360
Junyu Ke <libertyunionkjy@gmail.com>
2020-11-27 01:24:38 +0800

    https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/

--- LeetCode/src/offer/Main.java
+++ LeetCode/src/offer/Main.java
@@ -1,11 +1,11 @@
 package offer;
 
-import offer.FindNumberIn2DArray;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        MinArray solution = new MinArray();
-        solution
-        System.out.println();
+        int[] nums = {2, 7, 11, 15};
+        twoSum solution = new twoSum();
+        System.out.println(Arrays.toString(solution.twoSum(nums,19)));
     }
 }
--- LeetCode/src/offer/MinArray.java
+++ LeetCode/src/offer/MinArray.java
@@ -2,17 +2,22 @@ package offer;
 
 public class MinArray {
     public int minArray(int[] nums) {
-        int res = nums[0];
         if(nums.length == 1)
             return nums[0];
-        else{
-            for(int i = 0; i <= nums.length - 2; i++){
-                if(nums[i] > nums[i+1]){
-                    res = nums[i+1];
-                    break;
-                }
-            }
+        int left = 0, right = nums.length - 1;
+        if(nums[right] > nums[0])
+            return nums[0];
+          while(left <= right){
+            int mid = (left + right) / 2;
+            if(nums[mid] > nums[mid + 1])
+                return nums[mid + 1];
+            else if(nums[mid - 1] > nums[mid])
+                return nums[mid];
+            else if(nums[mid] > nums[0])
+                left = mid + 1;
+            else if(nums[mid] < nums[0])
+                right = mid - 1;
         }
-        return res;
+        return -1;
     }
 }
