14d9c96b0f6f68c3f145b493d17639eff259f7fb
Junyu Ke <libertyunionkjy@gmail.com>
2020-12-03 01:00:12 +0800

    202.快乐数

--- LeetCode/src/offer/Main.java
+++ LeetCode/src/offer/Main.java
@@ -2,9 +2,7 @@ package offer;
 
 public class Main {
     public static void main(String[] args) {
-        ListNode l1 = new ListNode(2);
-        ListNode l2 = new ListNode(5);
-        addTwoNumbers solution = new addTwoNumbers();
-        solution.addTwoNumbers(l1, l2);
+        isHappy solution = new isHappy();
+        System.out.println(solution.isHappy(19));
     }
 }
